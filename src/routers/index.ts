ff0a699d3570a02f86984ff963c6a1fe34a52219
v-liyr <liyoro@163.com>
2022-06-09 18:08:18 +0800

    路由配置

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,41 @@
 /*
  * @Author: liyoro
  * @since: 2022-03-24 17:00:31
- * @lastTime: 2022-05-26 14:46:19
+ * @lastTime: 2022-06-09 17:34:43
  */
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from './modules/index'
+import type { RouteRecordRaw } from 'vue-router'
+// import routes from './modules/index'
+
+// 导入modules中所有router
+const metaRouters = import.meta.globEager('./modules/*.ts')
+
+// 处理路由
+export const routerArray: RouteRecordRaw[] = []
+Object.keys(metaRouters).forEach((item) => {
+	Object.keys(metaRouters[item]).forEach((key: any) => {
+		routerArray.push(...metaRouters[item][key])
+	})
+})
+
+const publicRoutes: RouteRecordRaw[] = [
+	{
+		path: '/:pathMatch(.*)',
+		redirect: '/404'
+	},
+	{
+		path: '/404',
+		component: () => import('@/views/404.vue')
+	},
+	{
+		path: '/login',
+		component: () => import('@/views/login/index.vue'),
+		name: 'login',
+		meta: { title: '登录' }
+	}
+]
+
+const routes: RouteRecordRaw[] = [...publicRoutes, ...routerArray]
 
 // app router
 export const router = createRouter({
--- src/routers/modules/index.ts
+++ src/routers/modules/index.ts
@@ -1,57 +1,32 @@
 /*
  * @Author: liyoro
  * @since: 2022-03-25 11:19:59
- * @lastTime: 2022-06-01 15:52:02
+ * @lastTime: 2022-06-09 17:39:17
  */
 import type { RouteRecordRaw } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
-const PUBLIC_PATH = 'li'
-
-const publicRoutes: RouteRecordRaw[] = [
+const demoRouter: RouteRecordRaw[] = [
 	{
-		path: '/:pathMatch(.*)',
-		redirect: '/404'
-	},
-	{
-		path: '/404',
-		component: () => import('@/views/404.vue')
+		path: `/demo`,
+		component: Layout,
+		redirect: `/demo/demo1`,
+		meta: { title: '例子' },
+		children: [
+			{
+				path: `/demo/demo1`,
+				name: 'demo1',
+				component: () => import('@/views/demo/demo1/index.vue'),
+				meta: { title: 'demo1' }
+			},
+			{
+				path: `/demo/demo2`,
+				name: 'demo2',
+				component: () => import('@/views/demo/demo2/index.vue'),
+				meta: { title: 'demo2' }
+			}
+		]
 	}
 ]
 
-const routes: RouteRecordRaw[] = [
-	{
-		path: '/',
-		name: 'index',
-		redirect: `/${PUBLIC_PATH}`,
-		meta: {
-			title: '首页'
-		}
-	},
-	{
-		path: `/${PUBLIC_PATH}`,
-		name: `${PUBLIC_PATH}`,
-		component: Layout,
-		redirect: `/${PUBLIC_PATH}/home`,
-		meta: { title: '管理平台' },
-		children: [
-			{
-				path: `home`,
-				component: () => import('@/views/home/index.vue'),
-				name: 'home',
-				meta: {
-					title: '首页'
-				}
-			}
-		]
-	},
-	{
-		path: '/login',
-		component: () => import('@/views/login/index.vue'),
-		name: 'login',
-		meta: { title: '登录' }
-	},
-	...publicRoutes
-]
-
-export default routes
+export default demoRouter
